50799e9ac3793f8355369c1082b1c6f14cac5f47
ruwa <ruwankamadhushan@gmail.com>
2014-07-22 07:07:20 +0530

    first commit

---
+++ js/background.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Ruwa on 7/20/2014.
+ */
---
+++ js/popup.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    chrome.tabs.getSelected(null, function(tab) {
+        var urltoconvert = tab.url;
+        urltoconvert = fixedEncodeURIComponent(urltoconvert);
+        $('#qrimg').attr('src', 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + urltoconvert);
+        $('#spinner').css('display', 'none');
+    });
+
+    function fixedEncodeURIComponent (str) {
+        return encodeURIComponent(str).replace(/[!'()]/g, escape).replace(/\*/g, "%2A");
+    }
+
+});

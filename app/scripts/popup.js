1129e75626ce6a8c18ed490b25fdecabdd7101c8
ruwa <ruwankamadhushan@gmail.com>
2015-07-06 22:22:39 +0530

    add qr code download feature

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -6,13 +6,13 @@ $(document).ready(function () {
     chrome.tabs.getSelected(null, function (tab) {
         var urltoconvert = tab.url;
         urltoconvert = fixedEncodeURIComponent(urltoconvert);
-        console.log(urltoconvert);
-        $('#qrimg').attr('src', 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + urltoconvert);
+        var imgSrc = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + urltoconvert;
+        $('#qrimg').attr('src', imgSrc);
+        $('#download').attr('href', imgSrc).attr('download', tab.title + '.png');
         $('#spinner').css('display', 'none');
     });
 
     function fixedEncodeURIComponent(str) {
-        //return encodeURIComponent(str).replace(/[!'()]/g, escape).replace(/\*/g, '%2A');
         return encodeURIComponent(str).replace(/\-/g, '%2D').replace(/\_/g, '%5F').replace(/\./g, '%2E').replace(/\!/g, '%21').replace(/\~/g, '%7E').replace(/\*/g, '%2A').replace(/\'/g, '%27').replace(/\(/g, '%28').replace(/\)/g, '%29').replace(/\#/g, '%23');
     }
 

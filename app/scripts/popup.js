97cc904fcf3066adf6098d80da9ead461bf8ea5d
ruwa <ruwankamadhushan@gmail.com>
2015-07-14 16:00:25 +0530

    add chrome webstore branding badge

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -13,7 +13,17 @@ $(document).ready(function () {
     });
 
     function fixedEncodeURIComponent(str) {
-        return encodeURIComponent(str).replace(/\-/g, '%2D').replace(/\_/g, '%5F').replace(/\./g, '%2E').replace(/\!/g, '%21').replace(/\~/g, '%7E').replace(/\*/g, '%2A').replace(/\'/g, '%27').replace(/\(/g, '%28').replace(/\)/g, '%29').replace(/\#/g, '%23');
+        return encodeURIComponent(str)
+            .replace(/\-/g, '%2D')
+            .replace(/\_/g, '%5F')
+            .replace(/\./g, '%2E')
+            .replace(/\!/g, '%21')
+            .replace(/\~/g, '%7E')
+            .replace(/\*/g, '%2A')
+            .replace(/\'/g, '%27')
+            .replace(/\(/g, '%28')
+            .replace(/\)/g, '%29')
+            .replace(/\#/g, '%23');
     }
 
     $('#apilink').on('click', function () {
